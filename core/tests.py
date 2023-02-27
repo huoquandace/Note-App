5870162a8b4c5944d747c4811084cd69085fc3a0
Portgas D.Ace <huoquandace@gmail.com>
2023-02-27 12:58:02 +0700

    start build unit test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from core.models import Note
+
+
+class First(TestCase):
+
+    def setUp(self):
+        user = get_user_model().objects.create(username='username', password='password')
+        Note.objects.create(headline="Note 1", content="This is a test content", user=user)
+
+    def test_note_text(self):
+        note = Note.objects.get(headline="Note 1")
+        self.assertEqual(note.content, "This is a test content")
