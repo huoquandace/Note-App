942be852c465fc4a2223d89448a98f173e89fc00
Portgas D.Ace <huoquandace@gmail.com>
2023-03-03 17:42:58 +0700

    up

--- learn.js
+++ learn.js
@@ -67,6 +67,37 @@
             - NOTE: Event bubbling
             - Input, select, key up/down events
             - preventDefault, stopPropagation
+            - Event listener
+    17. JSON -> string
+        - JSON.stringify, JSON.parse
+    18. Promise (sync, async)
+    - pain: Callback hell
+    - new Promise
+    - Promise methods
 */
 
-var temp
+var users = [{id: 1,name: 'John Doe',},{id: 2,name: 'Jackson',},{id: 3,name: 'Alice',}]
+
+var comments = [{id: 1,text: 'This is a comment 1',user_id: 1,},{id: 2,text: 'This is a comment 2',user_id: 1,},{id: 3,text: 'This is a comment 3',user_id: 3,}]
+
+var getComments = () => new Promise((resolve) => setTimeout(() => {resolve(comments)}, 3000))
+
+var getUsersByIds = (userIds) => new Promise((resolve) => setTimeout(() => {
+    resolve(users.filter((user) => userIds.includes(user.id)))
+}, 5000))
+
+getComments()
+    .then(comments => {
+        var userIds  = comments.map(comment => comment.user_id)
+        return getUsersByIds(userIds)
+            .then(users => ({users: users, comments: comments}))
+    })
+    .then(data => {
+        var ul = document.querySelector('ul')
+        data.comments.forEach(comment => {
+            var user = data.users.find(user => user.id === comment.user_id)
+            ul.innerHTML += `<li>${user.name}: ${comment.text}</li>`
+        });
+    })
+
+
