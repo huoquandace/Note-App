9471aa027c0d146a36a4157d336e10567184a115
Portgas D.Ace <120877704+huoquandace@users.noreply.github.com>
2023-03-03 14:50:06 +0700

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,72 @@
+/*
+    1. Variable: var <variable_name>
+    2. Comment: // or / * * /. Shortcut: Ctrl / or Alt Shift A or Enter /**
+    3. Built-in function: console alert confirm prompt setTimeout setInterval
+    4. Operator: 
+        - Assignment Operators: =, +=, -=, *=, /=, %=, **=
+        - Arithmetic Operators: +, -, *, /, %, ++, --, **
+        - Comparison Operators: ==, !=, ===, !==, >, >=, <, <=
+        - Logical Operators: &&, ||, !
+        - String Operators: +, +=
+        - Bitwise Operators: &, |, ^, ~, <<, >>, >>>
+        - Other JavaScript Operators: typeof, ?: , in
+    5. Boolean: Falsy: 0, false, '', "", undefined, NaN, null => false. Truthy: other
+    6. Datatype:
+        - Primitive data: number, string, boolean, undefined, null, symbol
+        - Complex data: - Function
+                        - Object: Object, Array
+    7. Function: NOTE: arguments => all parameters of function
+        - Declaration function - hoisted
+        - Expression function
+    8. String:
+        - Method: length, indexOf, lastIndexOf, search, slice, replace, toUpperCase, toLowerCase, trim, split, charAt
+        - template string `${<variable_name>}`
+        - String includes
+    9. Number:
+        - type: IEEE-754, bigInt
+        - Parse
+        - e, toFixed
+    10. Array:
+        - length, []
+    11. Object:
+        - Object Constructor
+        - Object prototype
+        - Date, Math -> PI, round, abs, ceil, floor, random, min, max
+    12. Conditional statements: 
+        - if(){} else if(){} else{}
+        - switch(){}
+        - ternary operator: ?:
+    13: Loop
+        - For, For in (key), For of (value). Note: Object.keys(<object_name>), Object.value(<object_name>) -> array
+        - While, Do While
+        - Break, Continue, 
+        - Nested Loop
+        - Recursion (de quy): NOTE: Set
+    14. Array methods: forEach, every, some, find, filter, map, reduce
+        - Array includes
+    15. Callback
+    16. HTML DOM: (element, attribute, text)
+        - Get Node:
+            - Element: id, class, tagName, Css Selector, HTML Collection
+            - Attribute:
+                - setter: element.<valid_attr> = <value>
+                - getter: element.<valid_attr>
+                - element.getAttribute(<attr>)
+                - element.setAttribute(<attr>, <value>)
+            - Text:
+                - get: element.innerText, element.textContent
+                - set: element.innerText = <value>, element.textContent = <value>
+                - Diff: innerText just text
+        - InnerHTML, OuterHTML
+        - Node Properties
+        - DOM CSS
+        - ClassList property
+        - DOM events
+            - Attributes events
+            - Assign events using element nodes
+            - NOTE: Event bubbling
+            - Input, select, key up/down events
+            - preventDefault, stopPropagation
+*/
+
+var temp
